a3717620374327754f569047880decdf984a3f70
bj-eberhardt <57727932+bj-eberhardt@users.noreply.github.com>
2025-07-02 11:25:43 +0200

    Update screenshot.js

--- scripts/screenshot.js
+++ scripts/screenshot.js
@@ -1,7 +1,10 @@
 import puppeteer from 'puppeteer';
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: 'new' });
+  const browser = await puppeteer.launch({
+    headless: 'new',
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  });
   const page = await browser.newPage();
   await page.goto('http://localhost:8080', { waitUntil: 'networkidle0' });
   await page.screenshot({ path: 'screenshot.png', fullPage: true });
